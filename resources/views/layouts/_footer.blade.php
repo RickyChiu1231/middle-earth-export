251fa11b84da5483d934bc952c7186b8cbb48fb2
RickyChiu1231 <laiyin101020@gmail.com>
2019-07-25 11:37:16 +0000

    basiclayout

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <p class="float-left">
-      由 <a href="https://leo108.com" target="_blank">Leo</a> Design & Coding <span style="color: #e27575;font-size: 14px;">❤</span>
+      By <a href="https://laravel.com/" target="_blank">Ricky,Monk,Lee</a> Design & Coding <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
     <p class="float-right"><a href="mailto:laiyin101020@gmail.com">Contact Us</a></p>
