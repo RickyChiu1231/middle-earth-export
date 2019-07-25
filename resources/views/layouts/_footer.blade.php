6c1350ae7e5bb48679ed24ae245ff4d05150dfa2
RickyChiu1231 <laiyin101020@gmail.com>
2019-07-25 10:22:31 +0000

    initapp

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://leo108.com" target="_blank">Leo</a> Design & Coding <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:laiyin101020@gmail.com">Contact Us</a></p>
+  </div>
+</footer>
